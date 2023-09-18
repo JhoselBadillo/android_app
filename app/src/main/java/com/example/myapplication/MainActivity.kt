fa5194fa90a4813af4a665f5c8d95da232175f38
Erick Daniel Guzman Hernandez <edguzman@citivirt.citi.com.mx>
2023-09-18 12:18:51 -0600

    Ejemplo1 de Fragmets

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(FirstFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId){
+                R.id.fragment1 -> replaceFragment(FirstFragment())
+                R.id.fragment2 -> replaceFragment(SecondFragment())
+                R.id.fragment3 -> replaceFragment(ThirdFragment())
+
+            }
+            true
+
+        }
+
+
     }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
+    }
+
 }
